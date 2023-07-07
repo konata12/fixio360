980b57d41dc32fc476b1f71b7a17ac78fc92fe01
dima <87605146+konata12@users.noreply.github.com>
2023-07-07 20:13:17 +0300

    starting working on server

--- client/src/components/main/deseases/Deseases.jsx
+++ client/src/components/main/deseases/Deseases.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+
 const Deseases = () => {
+
+    useEffect(() => {
+        
+    })
+
     return (
         <section>
-            
+            <div className="container">
+                <p>dasds</p>
+            </div>
         </section>
     );
 }
