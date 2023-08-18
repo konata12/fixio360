fd6f36a16eaa90ce5d167c0f1d46264ddf32fb21
dima <87605146+konata12@users.noreply.github.com>
2023-08-19 01:06:06 +0300

    Update PageBtn.jsx

--- client/src/components/PageBtn.jsx
+++ client/src/components/PageBtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function PageBtn(num) {
+export default function PageBtn(page) {
     return (
         <button className='flex justify-center items-center text-white text-2xl py-1 px-1'>
-            num
+            {page}
         </button>
     )
 }
